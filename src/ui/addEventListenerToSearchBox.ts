8b0c27a207f7dce0cbde1bf1a31b16d975d80e73
Orlando Parisblue <orlando.parisblue@ailo.io>
2024-07-06 22:13:38 +1000

    Make search case-insensitive

--- src/ui/addEventListenerToSearchBox.ts
+++ src/ui/addEventListenerToSearchBox.ts
@@ -2,13 +2,14 @@ export function addEventListenerToSearchBox() {
   const searchBox = document.querySelector("#search") as HTMLInputElement;
 
   searchBox.addEventListener("input", () => {
+    const searchTerm = searchBox.value.toLowerCase();
     const allTextures = document.querySelectorAll("[data-searchable-name]");
 
     allTextures.forEach((item) => {
       if (!(item instanceof HTMLElement)) return;
 
-      const name = item.getAttribute("data-searchable-name");
-      const includedInSearch = name?.includes(searchBox.value);
+      const name = item.getAttribute("data-searchable-name")?.toLowerCase();
+      const includedInSearch = name?.includes(searchTerm);
 
       item.style.display = includedInSearch ? "block" : "none";
     });
